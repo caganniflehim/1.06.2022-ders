bafefe7e3590a0e90727a2adb425207744ba5691
caganniflehim <omercagand@gmail.com>
2023-11-18 17:53:40 +0300

    Initial commit

---
+++ 1.06.2022 ders/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace _1._06._2022_ders
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "message box(tamam)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "message box(tamam/iptal)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "evet hayır ünlem";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "evet hayır iptal";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(190, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "dur yeniden dene iptal";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 162);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(191, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "metin seç ve aç txt dosyası bul";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 192);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(190, 108);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 307);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(191, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "metin dosyası kaydet";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(210, 11);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(164, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "resim seç";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(210, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 230);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(210, 297);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 132);
+            this.listBox1.TabIndex = 10;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(354, 307);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(96, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "yeni isim";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Inputbox";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(354, 336);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(96, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "isim düzelt";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(416, 12);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(155, 23);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "klasor seç";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(427, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "klasör:";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(430, 71);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(269, 45);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "klasor içindeki dosyaları listele";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(430, 122);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(269, 132);
+            this.listBox2.TabIndex = 17;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(466, 261);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(240, 23);
+            this.button13.TabIndex = 18;
+            this.button13.Text = "formun rengi değiştir";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(466, 291);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(233, 23);
+            this.button14.TabIndex = 19;
+            this.button14.Text = "butonun rengi değiştir";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(466, 321);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(233, 23);
+            this.button15.TabIndex = 20;
+            this.button15.Text = "butonun yazı tipini değiştir";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+    }
+}
+
---
+++ 1.06.2022 ders/Form1.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._06._2022_ders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("mesax box", "selam", MessageBoxButtons.OK);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           // MessageBox.Show("merhaba", "selam", MessageBoxButtons.OKCancel);
+
+            // ikinci kısım
+           DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("programdan çıkılsın mı","çıkış",MessageBoxButtons.YesNo);
+            if (dialog==DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("çıkış yapılmadı");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("merhaba", "selam", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("merhaba", "selam", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("merhaba", "selam", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "metin dosyası | *.txt";
+            file.RestoreDirectory = true;//son açılan dizinin açık kalması için
+            if (file.ShowDialog()==DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(file.FileName);
+                richTextBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+            //2.yol
+            richTextBox1.LoadFile(file.FileName, RichTextBoxStreamType.PlainText);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog file = new SaveFileDialog();
+            file.DefaultExt = "*.txt";
+            file.Filter = "metin dosyası | *.txt";
+
+            if (file.ShowDialog()==DialogResult.OK && file.FileName.Length>0)
+            {
+                richTextBox1.SaveFile(file.FileName, RichTextBoxStreamType.PlainText);
+                MessageBox.Show("dosya kaydedildi", "kayıt yeri: "
+                    + file.FileName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "resim dosyası|*.jpg;*.png| video dosyası|*avi;|tüm dosyalar |*.*";
+            file.Title = "resim seç";
+            file.ShowDialog();
+            string filePath = file.FileName;
+            pictureBox1.ImageLocation = filePath;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string isim = Interaction.InputBox("isim giriş", "yeni isim", "örn:ali uzun");
+            listBox1.Items.Add(isim);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            string isim = Interaction.InputBox("isim girişi", "yeni isim", listBox1.SelectedItem.ToString());
+            listBox1.Items[index] = isim;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog()==DialogResult.OK)
+            {
+                string dosyayolu = folder.SelectedPath;
+                label2.Text = dosyayolu;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string dosyauolu = label2.Text;
+            if (dosyauolu!="")
+            {
+                foreach (string d in Directory.GetDirectories(dosyauolu))
+                {
+                    listBox2.Items.Add(Path.GetFileName(d));
+                }
+                foreach (string f in Directory.GetFiles(dosyauolu))
+                {
+                    listBox2.Items.Add(Path.GetFileName(f));
+                }
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if (color.ShowDialog() == DialogResult.OK)
+            {
+                button14.BackColor = color.Color;
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if (color.ShowDialog()==DialogResult.OK)
+            {
+                button14.BackColor = color.Color;
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            FontDialog font = new FontDialog();
+            font.ShowColor = true;
+            font.ShowDialog();
+            button15.Font = font.Font;
+            button15.ForeColor = font.Color;
+        }
+    }
+}
